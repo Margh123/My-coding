a80578900cb6e8b86f68b249398a45060663db7b
Margh123 <68310055+Margh123@users.noreply.github.com>
2021-11-26 20:44:06 +0700

    Create NumberProblem3.java

---
+++ NumberProblem3.java
@@ -0,0 +1,20 @@
+// Find all five-digit numbers (abcde) that satisfy a^e+b^d+c^c+d^b+e^a=abcde
+public class Flatform1 {
+	public static void main(String[] args) {
+		String s;
+		for (int i=10000;i<=99999;i++) {
+			s = Integer.toString(i);
+			if (s.charAt(2)=='0'||(s.charAt(1)=='0' && s.charAt(3)=='0')) {
+				continue;
+			}
+			if(Math.pow((int)((s.charAt(0))-48),(int)((s.charAt(4))-48))+
+			   Math.pow((int)((s.charAt(1))-48),(int)((s.charAt(3))-48))+
+			   Math.pow((int)((s.charAt(2))-48),(int)((s.charAt(2))-48))+
+			   Math.pow((int)((s.charAt(3))-48),(int)((s.charAt(1))-48))+
+			   Math.pow((int)((s.charAt(4))-48),(int)((s.charAt(0))-48))==(double)i) {
+				System.out.println(i);
+			}
+		}
+	}
+}
+// 48625
