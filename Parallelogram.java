f0d89ea8c590374f7ca3bbe049a8c7f6083c1909
Margh123 <68310055+Margh123@users.noreply.github.com>
2022-08-20 00:29:42 +0700

    Create Parallelogram.java

---
+++ Parallelogram.java
@@ -0,0 +1,32 @@
+public class ParallelogramPattern {
+    public static void main(String args[]) {
+
+     int w = 10; // How wide the parallelogram is
+     int h = 7; // How high the parallelogram is
+     
+     // Step 1: The Top
+      {
+          System.out.print(" ".repeat(h-1));
+          System.out.println("*".repeat(w));
+      }
+      
+      //Step 2: The Body
+       {
+           for(int k= 2; k<= h-1; k++){
+               System.out.print(" ".repeat(h-k));
+                 {
+                     System.out.print("*");
+                     System.out.print(" ".repeat(w-2));
+                     System.out.print("*");
+                 }
+                System.out.println(" ".repeat(k-1));
+           }
+       }
+       
+       // Step 3: The bottom
+       {
+           System.out.print("*".repeat(w));
+           System.out.print(" ".repeat(w));
+       }
+    }
+}
